0b3be5e1509770d239811d02e2dd325ed154c17e
Farzan <farzan@OFD001M6014.local>
2025-11-29 00:21:19 +0400

    add tiny htmls

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:month>', views.monthly_challenge_by_number), # order matters
-    path('<str:month>', views.monthly_challenge),
+    path('<str:month>', views.monthly_challenge, name='month-chall'),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     'jan': 'snowww',
@@ -18,16 +19,17 @@ monthly_challenges = {
 
 def monthly_challenge(request, month):
     try:
-        return HttpResponse(monthly_challenges[month])
+        return HttpResponse(f'<h1>{monthly_challenges[month]}</h1>')
     except:
-        return HttpResponseNotFound("Not supported!")
+        return HttpResponseNotFound('<h1>Not supported!</h1>')
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     if month > len(month):
-        return HttpResponseNotFound("Not supported!")
+        return HttpResponseNotFound('<h1>Not supported!</h1>')
     
     target_month = months[month - 1]
+    redirect_path = reverse('month-chall', args=[target_month]) # result: /chall/jan
 
-    return HttpResponseRedirect('/chall/' + target_month)
+    return HttpResponseRedirect(redirect_path)
